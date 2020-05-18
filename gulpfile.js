faabc60e498ed20cfd43848a7e95746fc3151bbf
tsdiokno <tsdiokno@gmail.com>
2020-05-18 14:41:32 +0800

    Makailang ulit para sa workbox caching.

--- gulpfile.js
+++ gulpfile.js
@@ -10,32 +10,12 @@ gulp.task("clean", function () {
 gulp.task("hugo-build", shell.task(["hugo"]));
 
 gulp.task('service-worker', () => {
-  // This will return a Promise
   return workbox.generateSW({
     globDirectory: './docs',
     globPatterns: [
       '**/*.{html,json,js,css}',
     ],
     swDest: './docs/sw.js',
-
-    // Define runtime caching rules.
-    runtimeCaching: [{
-      // Match any request that ends with .png, .jpg, .jpeg or .svg.
-      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-      // Apply a cache-first strategy.
-      handler: 'CacheFirst',
-
-      options: {
-        // Use a custom cache name.
-        cacheName: 'images',
-
-        // Only cache 10 images.
-        expiration: {
-          maxEntries: 10,
-        },
-      },
-    }],
   });
 });
 
